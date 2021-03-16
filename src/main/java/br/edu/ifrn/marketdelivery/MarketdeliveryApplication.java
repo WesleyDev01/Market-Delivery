bf0746063a9632723b54f3f6d2a3d49bbadfc23e
WesleyDev01 <josewesley12040@gmail.com>
2021-03-16 17:26:25 -0300

    first commit

---
+++ src/main/java/br/edu/ifrn/marketdelivery/MarketdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.marketdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrn/marketdelivery/MarketdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.marketdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
