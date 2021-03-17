c9a7c0eec662a3aded0aeef03f71a2253080ed73
WesleyDev01 <josewesley12040@gmail.com>
2021-03-17 19:42:40 -0300

    colocando dados na tela inicial da aplicacao

--- src/main/java/br/edu/ifrn/marketdelivery/controllers/ComercioController.java
+++ src/main/java/br/edu/ifrn/marketdelivery/controllers/ComercioController.java
@@ -1,10 +1,13 @@
 package br.edu.ifrn.marketdelivery.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifrn.marketdelivery.models.Comercio;
 import br.edu.ifrn.marketdelivery.repositories.ComercioRepository;
@@ -25,7 +28,15 @@ public class ComercioController {
 	public String cadastrarComerio(Comercio comercio) {
 		System.out.println(comercio);
 		cr.save(comercio);
-		return "home";
+		return "redirect:/comercios";
+	}
+
+	@GetMapping
+	public ModelAndView listarComercios() {
+		List<Comercio> comercios = cr.findAll();
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("comercios", comercios);
+		return mv;
 	}
 
 }
