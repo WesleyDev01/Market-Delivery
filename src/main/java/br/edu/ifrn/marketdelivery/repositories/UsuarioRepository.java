81d81b2b4d6519649783a2d33419d3336686caf5
WesleyDev01 <josewesley12040@gmail.com>
2021-03-17 05:48:00 -0300

    criando funcao de cadastrar usuario

---
+++ src/main/java/br/edu/ifrn/marketdelivery/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifrn.marketdelivery.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ifrn.marketdelivery.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
