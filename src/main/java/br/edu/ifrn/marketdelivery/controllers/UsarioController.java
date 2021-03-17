e114a83a3bc5764833466509fbd7d0d31def8c25
WesleyDev01 <josewesley12040@gmail.com>
2021-03-17 10:30:29 -0300

    criando formulario para cadastrar usuario

--- src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
+++ src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
@@ -1,6 +1,8 @@
 package br.edu.ifrn.marketdelivery.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,12 +13,19 @@ import br.edu.ifrn.marketdelivery.repositories.UsuarioRepository;
 @RequestMapping("/usuarios")
 public class UsarioController {
 
-	UsuarioRepository ur;
+	@Autowired
+	private UsuarioRepository ur;
+
+	@GetMapping("/cadastrarUsuario")
+	public String formUser(Usuario usuario) {
+		return "usuarios/formUsuario";
+	}
 
 	@PostMapping
 	public String cadastrarUsuario(Usuario usuario) {
+		System.out.println(usuario);
 		ur.save(usuario);
-		return "redirect:/usuarios";
+		return "home";
 	}
 
 }
