0940a6106b3479a32a604893ab10a0e1aa120d6e
WesleyDev01 <josewesley12040@gmail.com>
2021-03-18 20:01:58 -0300

    criando a lista de usuarios

--- src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
+++ src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
@@ -1,10 +1,13 @@
 package br.edu.ifrn.marketdelivery.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifrn.marketdelivery.models.Usuario;
 import br.edu.ifrn.marketdelivery.repositories.UsuarioRepository;
@@ -25,7 +28,15 @@ public class UsarioController {
 	public String cadastrarUsuario(Usuario usuario) {
 		System.out.println(usuario);
 		ur.save(usuario);
-		return "home";
+		return "redirect:/comercios";
+	}
+
+	@GetMapping("/listaUsuarios")
+	public ModelAndView listarUsuarios() {
+		List<Usuario> usuarios = ur.findAll();
+		ModelAndView mv = new ModelAndView("usuarios/listaUsuarios");
+		mv.addObject("usuarios", usuarios);
+		return mv;
 	}
 
 }
