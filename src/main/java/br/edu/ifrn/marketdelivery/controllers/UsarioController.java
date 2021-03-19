9654e6cfd7223120a6c47786bb3b4282362bb91b
WesleyDev01 <josewesley12040@gmail.com>
2021-03-18 21:30:45 -0300

    criando a tela de perfil do usuario

--- src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
+++ src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
@@ -1,10 +1,12 @@
 package br.edu.ifrn.marketdelivery.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,6 +39,24 @@ public class UsarioController {
 		ModelAndView mv = new ModelAndView("usuarios/listaUsuarios");
 		mv.addObject("usuarios", usuarios);
 		return mv;
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView visualizarPerfil(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Usuario> opt = ur.findById(id);
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/comercios");
+			return md;
+		}
+
+		md.setViewName("usuarios/perfilUsuario");
+		Usuario usuario = opt.get();
+
+		md.addObject("usuario", usuario);
+
+		return md;
+
 	}
 
 }
