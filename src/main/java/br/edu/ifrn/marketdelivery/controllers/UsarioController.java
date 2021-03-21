8f1ff38bb308f88452c4ade6f035819f32a1575c
WesleyDev01 <josewesley12040@gmail.com>
2021-03-21 20:00:15 -0300

    fazendo melhorias na aplicacao e separando tipos de usuario para cada funcao

--- src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
+++ src/main/java/br/edu/ifrn/marketdelivery/controllers/UsarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.edu.ifrn.marketdelivery.models.Comercio;
+import br.edu.ifrn.marketdelivery.models.PacoteDeCompra;
+import br.edu.ifrn.marketdelivery.models.Papel;
 import br.edu.ifrn.marketdelivery.models.Usuario;
+import br.edu.ifrn.marketdelivery.repositories.ComercioRepository;
+import br.edu.ifrn.marketdelivery.repositories.PacoteDeCompraRepository;
+import br.edu.ifrn.marketdelivery.repositories.PapelRepository;
 import br.edu.ifrn.marketdelivery.repositories.UsuarioRepository;
 
 @Controller
@@ -29,6 +36,12 @@ public class UsarioController {
 
 	@Autowired
 	private UsuarioRepository ur;
+	@Autowired
+	private PapelRepository papr;
+	@Autowired
+	private PacoteDeCompraRepository pacr;
+	@Autowired
+	private ComercioRepository cr;
 
 	private void buscarUsuarioLogado() {
 		Authentication autenticado = SecurityContextHolder.getContext().getAuthentication();
@@ -44,14 +57,25 @@ public class UsarioController {
 	}
 
 	@PostMapping
-	public String cadastrarUsuario(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
+	public String cadastrarUsuario(@Valid Usuario usuario, Papel papel, BindingResult result,
+			RedirectAttributes attributes) {
 
 		if (result.hasErrors()) {
 			return formUser(usuario);
 		}
 
-		System.out.println(usuario);
+		String senhaUsuario = usuario.getSenha();
+		usuario.setSenha(new BCryptPasswordEncoder().encode(senhaUsuario));
+
 		ur.save(usuario);
+
+		System.out.println(usuario);
+
+		Long usuario_id = usuario.getId();
+		Long papeis_id = (long) usuario.getTipo();
+
+		papr.inserirPapel(usuario_id, papeis_id);
+
 		attributes.addFlashAttribute("mensagem", "Usuário cadastrado com sucesso!");
 		return "redirect:/comercios";
 	}
@@ -63,25 +87,43 @@ public class UsarioController {
 		mv.addObject("usuarios", usuarios);
 		return mv;
 	}
-
-	@GetMapping("/{id}")
-	public ModelAndView visualizarPerfil(@PathVariable Long id) {
+	
+	@GetMapping("/listaUsuarios/{idUsuario}")
+	public String removerUsuario(@PathVariable Long idUsuario, RedirectAttributes attributes) {
 		buscarUsuarioLogado();
-		id = usuario.getId();
-		ModelAndView md = new ModelAndView();
-		//Optional<Usuario> opt = ur.findById(id);
-		//if (opt.isEmpty()) {
-		//	md.setViewName("redirect:/comercios");
-		//	return md;
-		//}
+		Optional<Usuario> opt = ur.findById(idUsuario);
+		if (!opt.isEmpty()) {
+			Usuario usuario = opt.get();
+			String cpfUsuario = usuario.getCpf();
+			List<PacoteDeCompra> compras = pacr.findByUsuario(usuario);
+			List<Comercio> comercios = cr.findByCpfUsuario(cpfUsuario);
+			pacr.deleteAll(compras);
+			cr.deleteAll(comercios);
+			ur.delete(usuario);
+			attributes.addFlashAttribute("mensagem", "Usuário removido!");
+		}
+		return "redirect:/usuarios/listaUsuarios";
+	}
 
-		md.setViewName("usuarios/perfilUsuario");
-		//Usuario usuario = opt.get();
+	@GetMapping("/listaCompras")
+	public ModelAndView listarCompras() {
+		buscarUsuarioLogado();
+		// List<PacoteDeCompra> pacotes = pacr.findAll();
+		List<PacoteDeCompra> pacotes = pacr.findByUsuario(usuario);
+		ModelAndView mv = new ModelAndView("usuarios/listaCompras");
+		mv.addObject("pacotes", pacotes);
+		return mv;
+	}
 
-		md.addObject("usuario", usuario);
-
-		return md;
-
+	@GetMapping("/listaCompras/{idCompra}")
+	public String removerCompra(@PathVariable Long idCompra, RedirectAttributes attributes) {
+		Optional<PacoteDeCompra> opt = pacr.findById(idCompra);
+		if (!opt.isEmpty()) {
+			PacoteDeCompra pacote = opt.get();
+			pacr.delete(pacote);
+			attributes.addFlashAttribute("mensagem", "Compra cancelada com sucesso!");
+		}
+		return "redirect:/usuarios/listaCompras";
 	}
 
 }
--- src/main/java/br/edu/ifrn/marketdelivery/repositories/ComercioRepository.java
+++ src/main/java/br/edu/ifrn/marketdelivery/repositories/ComercioRepository.java
@@ -1,9 +1,13 @@
 package br.edu.ifrn.marketdelivery.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifrn.marketdelivery.models.Comercio;
 
 public interface ComercioRepository extends JpaRepository<Comercio, Long> {
 
+	List<Comercio> findByCpfUsuario (String cpfUsuario);
+	
 }
